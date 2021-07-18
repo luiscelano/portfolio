ea6c9c1379c6ba08ef866ccbaa9665031dbdd215
luiscelanomrb <luiscelano@twowheelstogo.com>
2021-07-18 17:46:22 -0600

    Updates

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const port = process.env.PORT||3000
+app.use(express.static(__dirname))
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'index.html'))
+})
+http.createServer(app).listen(port,()=>console.log(`listening on port ${port}`))
